0ad0bae7e9e7978836e0c75f96a8c1bbbfc6d896
Egor Romenko <go2003x@yandex.ru>
2025-05-06 03:59:08 +0300

    Add files via upload

---
+++ speech_recognition_module.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+
+def recognize_speech_from_mic():
+    recognizer = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("Пожалуйста, говорите...")
+        
+        #Настраиваем распознаватель для фонового шума
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        
+        audio = recognizer.listen(source)
+        
+        print("Обработка вашего голоса...")
+        
+        try:
+            #Используем Google Web Speech API для распознавания
+            text = recognizer.recognize_google(audio, language="ru-RU")
+            return(text)
+            
+        except sr.UnknownValueError:
+            return("Извините, я не смог распознать вашу речь")
+        except sr.RequestError as e:
+            return(f"Ошибка сервиса распознавания; {e}")
+
+if __name__ == "__main__":
+    print(recognize_speech_from_mic())
